7c55c811dc68f9dae354fab9a7fb1e713f32514d
filfai <filfai1003@gmail.com>
2024-10-22 00:36:17 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/gproject/main/Initializer.java
+++ src/main/java/com/gproject/main/Initializer.java
@@ -10,50 +10,39 @@ public class Initializer {
     private long window;
 
     public void init() {
-        // Inizializza GLFW
         if (!glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
 
-        // Specifica la versione di OpenGL: 3.3 con profilo di compatibilità
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
-        // Crea una finestra di 800x800
         window = glfwCreateWindow(800, 800, "2D Game", 0, 0);
         if (window == 0) {
             throw new RuntimeException("Unable to create the GLFW window");
         }
 
-        // Imposta il contesto corrente
         glfwMakeContextCurrent(window);
 
-        // Crea le capacità OpenGL (richiede un contesto attivo)
         GL.createCapabilities();
 
-        // Abilita il blending per la trasparenza
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        // Imposta il colore di default per la pulizia del buffer (nero trasparente)
         glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 
-        // Imposta la proiezione ortografica per il rendering 2D
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         glOrtho(0, 800, 0, 800, -1, 1);  // Coordinate 2D
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
 
-        // Abilita il rendering delle texture
         glEnable(GL_TEXTURE_2D);
 
-        // Ottieni le informazioni sul monitor principale
         long monitor = glfwGetPrimaryMonitor();
         GLFWVidMode videoMode = glfwGetVideoMode(monitor);
 
-        // Se possibile, centra la finestra nello schermo
         if (videoMode != null) {
             int screenWidth = videoMode.width();
             int screenHeight = videoMode.height();
